3b8b905f46500b5d3a67f4bc92c8bbc03c79e199
Ariska Hidayat <ariska138@gmail.com>
2020-10-28 20:56:39 +0700

    fix path for root

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import {BrowserRouter, Route, Switch} from "react-router-dom";
-import SignIn from "./components/auth/SignIn"
-import SignUp from './components/auth/SignUp';
-import Dashboard from './components/dashboard/Dashboard';
-import Home from './components/home/Home';
-import NavBar from './components/layout/NavBar';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import Dashboard from "./components/dashboard/Dashboard";
+import Home from "./components/home/Home";
+import NavBar from "./components/layout/NavBar";
 //notification pop up
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 function App() {
   return (
     <BrowserRouter>
-    <ToastContainer/>
-    <NavBar/>
-    <Switch>
-    <Route path="/signin" component={SignIn}/>
-    <Route path="/signup" component={SignUp}/>
-    <Route path="/dashboard" component={Dashboard}/>
-    <Route path="/" component={Home}/>
-    </Switch>
+      <ToastContainer />
+      <NavBar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
     </BrowserRouter>
-   
   );
 }
 
